5a8b652d6a8f63677252b60b625d8060041579c4
Temik-hub <surshkov.art@gmail.com>
2022-04-18 15:06:26 +0300

    исправляет ошибки меню

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const burger = document.querySelector('.header__burger')
 const burgerLine = document.querySelector('.header__burger-line')
 const menu = document.querySelector('.header__navigation')
 
-burger.onclick = function() {
+
+burger.addEventListener('click', function() {
   burgerLine.classList.toggle('header__burger-line--open')
   menu.classList.toggle('header__navigation--open')
-}
+  })
